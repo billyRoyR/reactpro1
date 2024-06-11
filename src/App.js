a9df514160271410fe792b400c4ba57e47bae7ee
billyRoyR <156974090+billyRoyR@users.noreply.github.com>
2024-06-11 01:27:12 +0100

    nav complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { Component } from "react";
 import { MenuData } from './MenuData';
+import './NavbarStyles.css';
 
 class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
     render () {
        return (
         <nav className="NavbarItems">
-            <h1>React <i className="fab fa-react"></i></h1>
-            <ul>
+            <h1 className="logo">React <i className="fab fa-react"></i></h1>
+            <div className="menu-icons" onClick={this.handleClick}>
+                <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                 {MenuData.map((item, index) => {
                     return (
                         <li key={index}>
